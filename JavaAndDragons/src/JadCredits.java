d87b45b43389adda2b7ee30fe6bcd5e77e5e1889
RAPEstudios <rapestudios@ok.de>
2016-02-23 21:12:11 +0100

    Credits programmiert. Language Klasse angelegt, für Sprachen Änderungen(vielleicht) JadCredits ThreadSystem überarbeiten. Springt nicht in run methode rein

---
+++ JavaAndDragons/src/JadCredits.java
@@ -0,0 +1,149 @@
+import java.awt.Font;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.RenderingHints;
+import java.awt.image.BufferedImage;
+
+public class JadCredits implements Runnable{
+
+	
+
+	public void drawJavaToAscII() {
+		 int width = 100;
+		 int height = 30;
+		 
+		 BufferedImage image = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
+		 Graphics g = image.getGraphics();
+		 g.setFont(new Font("SansSerif", Font.BOLD, 16));
+		 
+		 Graphics2D graphics = (Graphics2D) g;
+		 graphics.setRenderingHint(RenderingHints.KEY_TEXT_ANTIALIASING, RenderingHints.VALUE_TEXT_ANTIALIAS_ON);
+		 graphics.drawString("JAVA", 10, 20);
+		 
+		 for (int i = 0; i < height; i++) {
+			StringBuilder sb = new StringBuilder();
+			for (int j = 0; j < width; j++) {
+				sb.append(image.getRGB(j, i) == -16777216 ? " " : "E");
+				
+			}
+			if (sb.toString().trim().isEmpty()) {
+			continue;
+			
+			}
+			System.out.println(sb);
+		}
+	}
+	
+	public void drawAndToAscII(){
+		 int width = 100;
+		 int height = 30;
+		 
+		 BufferedImage image = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
+		 Graphics g = image.getGraphics();
+		 g.setFont(new Font("SansSerif", Font.BOLD, 16));
+		 
+		 Graphics2D graphics = (Graphics2D) g;
+		 graphics.setRenderingHint(RenderingHints.KEY_TEXT_ANTIALIASING, RenderingHints.VALUE_TEXT_ANTIALIAS_ON);
+		 graphics.drawString("AND", 10, 20);
+		 
+		 for (int i = 0; i < height; i++) {
+			StringBuilder sb = new StringBuilder();
+			for (int j = 0; j < width; j++) {
+				sb.append(image.getRGB(j, i) == -16777216 ? " " : "P");
+				
+			}
+			if (sb.toString().trim().isEmpty()) {
+			continue;
+			
+			}
+			System.out.println(sb);
+		}
+		
+	}
+	
+	public void drawDragonsToascII(){
+		int width = 100;
+		 int height = 30;
+		 
+		 BufferedImage image = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
+		 Graphics g = image.getGraphics();
+		 g.setFont(new Font("SansSerif", Font.BOLD, 16));
+		 
+		 Graphics2D graphics = (Graphics2D) g;
+		 graphics.setRenderingHint(RenderingHints.KEY_TEXT_ANTIALIASING, RenderingHints.VALUE_TEXT_ANTIALIAS_ON);
+		 graphics.drawString("DRAGONS", 10, 20);
+		 
+		 for (int i = 0; i < height; i++) {
+			StringBuilder sb = new StringBuilder();
+			for (int j = 0; j < width; j++) {
+				sb.append(image.getRGB(j, i) == -16777216 ? " " : "R");
+				
+			}
+			if (sb.toString().trim().isEmpty()) {
+			continue;
+			
+			}
+			System.out.println(sb);
+		}
+		
+	}
+	
+	/*
+	 * Muss hier nochmal gucken... 
+	 * er springt nicht in die run methode rein. obwohl ich den thread starte.
+	 */
+	public void printCredits() {
+		int sleepTime = 2000;
+		try {
+			System.out.println(JadLanguage.Deutsch.gameName);
+			Thread.sleep(sleepTime);
+			System.out.println("is presented by");
+			Thread.sleep(sleepTime);
+			System.out.println(JadLanguage.Deutsch.rapeStudios);
+			Thread.sleep(sleepTime);
+			System.out.println("programmed by");
+			Thread.sleep(sleepTime);
+			System.out.println(JadLanguage.Deutsch.firstProgrammer);
+			Thread.sleep(sleepTime);
+			System.out.println("and by");
+			Thread.sleep(sleepTime);
+			System.out.println(JadLanguage.Deutsch.secondProgrammer);
+			Thread.sleep(sleepTime);
+		} catch (Exception e) {
+			System.out.println("Something went wrong. Please restart the game. \n "
+					+ e);
+		}
+//		Thread t = new Thread(this, "Credits");
+//		t.start();
+		
+	}
+	public void run() {
+		
+	}
+//	public  void run() {
+//		int sleepTime = 2000;
+//		try {
+//			System.out.println(JadLanguage.Deutsch.gameName);
+//			Thread.sleep(sleepTime);
+//			System.out.println("is presented by");
+//			Thread.sleep(sleepTime);
+//			System.out.println(JadLanguage.Deutsch.rapeStudios);
+//			Thread.sleep(sleepTime);
+//			System.out.println("programmed by");
+//			Thread.sleep(sleepTime);
+//			System.out.println(JadLanguage.Deutsch.firstProgrammer);
+//			Thread.sleep(sleepTime);
+//			System.out.println("and by");
+//			Thread.sleep(sleepTime);
+//			System.out.println(JadLanguage.Deutsch.secondProgrammer);
+//			Thread.sleep(sleepTime);
+//		} catch (Exception e) {
+//			System.out.println("Something went wrong. Please restart the game. \n "
+//					+ e);
+//		}
+//		
+//		
+//		
+//	}
+	
+}
--- JavaAndDragons/src/JadMain.java
+++ JavaAndDragons/src/JadMain.java
@@ -1,143 +1,143 @@
+import java.io.IOException;
+
 public class JadMain {
 
 	public static void main(String[] args) {
 		
+		JadCredits credits = new JadCredits();
+
 		java.util.Scanner scan = new java.util.Scanner(System.in);
-		
+
 		boolean back = true;
 		String sel1, sel3;
 		int sel2;
-		
-		
+
 		JadPlayer p1 = new JadPlayer();
 		JadPlayer p2 = new JadPlayer();
 		JadPlayer p3 = new JadPlayer();
 		JadPlayer p4 = new JadPlayer();
-		
-		while(true){
-			if (back){
-				System.out.println("-----------------------------\n"
-								 + " Welcome to Java and Dragons \n"
-							 	 + "-----------------------------\n"
-							 	 + "(s) Start\n"
-							 	 + "(o) Options\n"
-							 	 + "(c) Credits\n"
-							 	 + "(e) Exit");
-			back = false;
+
+		while (true) {
+			if (back) {
+				System.out.println("-----------------------------\n" + " Welcome to Java and Dragons \n"
+						+ "-----------------------------\n" + "(s) Start\n" + "(o) Options\n" + "(c) Credits\n"
+						+ "(e) Exit");
+				back = false;
 			}
-			
-			
-			switch(scan.nextLine().charAt(0)){
-			case 's':{
-				
+
+			switch (scan.nextLine().charAt(0)) {
+			case 's': {
+
 				System.out.println("How many players are going to participate? [1-4]");
-				
+
 				sel1 = scan.nextLine();
-				while(!(sel1.equals("1")||sel1.equals("2")||sel1.equals("3")||sel1.equals("4"))){
+				while (!(sel1.equals("1") || sel1.equals("2") || sel1.equals("3") || sel1.equals("4"))) {
 					System.out.println("Please choose a whole number from 1 to 4 to select!");
 					sel1 = scan.nextLine();
 				}
 				sel2 = Integer.parseInt(sel1);
-				for (int i = 1; i<=sel2; i++){
-					System.out.println("What's your name, player "+i+"?");
+				for (int i = 1; i <= sel2; i++) {
+					System.out.println("What's your name, player " + i + "?");
 					sel1 = scan.nextLine();
-					while(sel1.length()>12||sel1.length()<3){
+					while (sel1.length() > 12 || sel1.length() < 3) {
 						System.out.println("Please choose a name with a length from 3 to 12 characters!");
 						sel1 = scan.nextLine();
 					}
-					
-					System.out.println("-----------------------------\n"
-									 + " Please choose a class, "+sel1+"!\n"
-									 + "-----------------------------\n"
-									 + "(0) Warrior\n"
-									 + "(1) Archer\n"
-									 + "(2) Wizard\n"
-									 + "(3) Rogue");
+
+					System.out.println("-----------------------------\n" + " Please choose a class, " + sel1 + "!\n"
+							+ "-----------------------------\n" + "(0) Warrior\n" + "(1) Archer\n" + "(2) Wizard\n"
+							+ "(3) Rogue");
 					sel3 = scan.nextLine();
-					while(!(sel3.equals("0")||sel3.equals("1")||sel3.equals("2")||sel3.equals("3"))){
-					System.out.println("Please choose a whole number from 0 to 3 to select!");
-					sel3 = scan.nextLine();
+					while (!(sel3.equals("0") || sel3.equals("1") || sel3.equals("2") || sel3.equals("3"))) {
+						System.out.println("Please choose a whole number from 0 to 3 to select!");
+						sel3 = scan.nextLine();
 					}
-					switch(i){
-					case 1:{p1.JadPlayer(sel1,Integer.parseInt(sel3));break;}
-					case 2:{p2.JadPlayer(sel1,Integer.parseInt(sel3));break;}
-					case 3:{p3.JadPlayer(sel1,Integer.parseInt(sel3));break;}
-					case 4:{p4.JadPlayer(sel1,Integer.parseInt(sel3));break;}
+					switch (i) {
+					case 1: {
+						p1.JadPlayer(sel1, Integer.parseInt(sel3));
+						break;
+					}
+					case 2: {
+						p2.JadPlayer(sel1, Integer.parseInt(sel3));
+						break;
+					}
+					case 3: {
+						p3.JadPlayer(sel1, Integer.parseInt(sel3));
+						break;
+					}
+					case 4: {
+						p4.JadPlayer(sel1, Integer.parseInt(sel3));
+						break;
+					}
 					}
 				}
-				
-				
-				System.out.println("Is this correct? (y/n)\n"
-								 + " -Playername- | -Class- ");
-				
-				switch(sel2){
-				case 4:{
+
+				System.out.println("Is this correct? (y/n)\n" + " -Playername- | -Class- ");
+
+				switch (sel2) {
+				case 4: {
 					sel1 = "             ".substring(p4.name.length());
-					System.out.printf (" %s"+sel1+"| %s \n", p4.name, p4.toS(0));
+					System.out.printf(" %s" + sel1 + "| %s \n", p4.name, p4.toS(0));
 				}
-				case 3:{
+				case 3: {
 					sel1 = "             ".substring(p3.name.length());
-					System.out.printf (" %s"+sel1+"| %s \n", p3.name, p3.toS(0));
+					System.out.printf(" %s" + sel1 + "| %s \n", p3.name, p3.toS(0));
 				}
-				case 2:{
+				case 2: {
 					sel1 = "             ".substring(p2.name.length());
-					System.out.printf (" %s"+sel1+"| %s \n", p2.name, p2.toS(0));
+					System.out.printf(" %s" + sel1 + "| %s \n", p2.name, p2.toS(0));
 				}
 				}
 				sel1 = "             ".substring(p1.name.length());
-				System.out.printf (" %s"+sel1+"| %s \n", p1.name, p1.toS(0));
-				
+				System.out.printf(" %s" + sel1 + "| %s \n", p1.name, p1.toS(0));
+
 				sel1 = scan.nextLine();
-				while(!(sel1.equals("y")||sel1.equals("n"))){
+				while (!(sel1.equals("y") || sel1.equals("n"))) {
 					System.out.println("Please type (y) for yes, or (n) for no");
 					sel1 = scan.nextLine();
 				}
-				if(sel1.equals("n")){
+				if (sel1.equals("n")) {
 					break;
-				}else{
+				} else {
 					System.out.println("Starting adventure...");
 					JadMap.drawMap();
-					
-					
-					
-					
-					
-					
-					
+
 				}
-				
-				break;	//end of menu-case start
+
+				break; // end of menu-case start
 			}
-			case 'o':{
-				System.out.println("Work in Progress =)\n"
-								 + "(any letter) Back");
+			case 'o': {
+				System.out.println("Work in Progress =)\n" + "(any letter) Back");
 				scan.nextLine();
 				back = true;
 				continue;
 			}
-			case 'c':{
+			case 'c': {
+				
+				credits.drawJavaToAscII(); // print JAVA with E-Letters
+				credits.drawAndToAscII(); // print AND with P-Letters
+				credits.drawDragonsToascII(); // print DRAGONS with R-Letters
+//				credits.printCredits(); //print the credits
+				
+
+				System.out.println("Press any key to get back");
+				scan.nextLine();
+				back = true;
 				break;
 			}
-			case 'e':{
+			case 'e': {
 				scan.close();
 				System.exit(0);
-				
+
 			}
-			
-			default:{
+
+			default: {
 				continue;
 			}
-			
+
 			}
 		}
-		
-		
-		
-		
-		
-		
-		
-		
+
 	}
 
 }
